e556e41e685e05230b43f8b71cc2f0973faef4aa
Lenin Chavez <102402199+Hyp3Boy@users.noreply.github.com>
2024-09-22 20:28:09 -0500

    feat: add new spotify dataset

--- helpers/spotify_eda.py
+++ helpers/spotify_eda.py
@@ -2,68 +2,56 @@ import numpy as np
 import pandas as pd
 
 # general
-delimiter = "\n" + "-" * 50;
+delimiter = "\n" + "-" * 50
 
 # data analysis for spotify
-df = pd.read_csv('./data/spotify_data.csv', encoding = "ISO-8859-1");
+df = pd.read_csv("./data/tracks_features.csv", encoding="ISO-8859-1")
 
-print(delimiter);
-print("Dataframe information:");
-print(df.info());
+print(delimiter)
+print("Dataframe information:")
+print(df.info())
 
 # delete unnecessary columns
-columns_to_drop = [
-    "Spotify Playlist Count",
-    "Spotify Playlist Reach",
-    "YouTube Likes",
-    "YouTube Playlist Reach",
-    "Apple Music Playlist Count",
-    "Deezer Playlist Count",
-    "Deezer Playlist Reach",
-    "Amazon Playlist Count",
-    "Pandora Track Stations",
-    "TikTok Posts",
-    "TikTok Likes",
-    "TIDAL Popularity",
-];
+columns_to_maintain = [
+    "id",
+    "name",
+    "album",
+    "album_id",
+    "artists"
+]
 
 # remove columns and improve format
-df = df.drop(columns_to_drop, axis = 1);
-df.columns = [col.replace(" ", "_").lower() for col in df.columns];
+df = df.filter(items=columns_to_maintain)
+df.columns = [col.replace(" ", "_").lower() for col in df.columns]
 
-# remove null values2. 
-df = df.dropna(how = "any", axis = 0);
+# remove null values2.
+df = df.dropna(how="any", axis=0)
 
-print(delimiter);
-print("Dataframe information after cleanup:");
-print(df.info());
+# remove if any column has a "," in it
+df = df.replace(",", "", regex=True)
 
-print(delimiter);
-print("Dataframe rows non-null:", df.shape[0]);
-print("Dataframe cols non-null:", df.shape[1]);
+print(delimiter)
+print("Dataframe information after cleanup:")
+print(df.info())
 
-# data type conversion
-int_columns = [
-    'all_time_rank', 'track_score', 'spotify_streams', 'spotify_popularity',
-    'youtube_views', 'tiktok_views', 'airplay_spins', 'siriusxm_spins',
-    'pandora_streams', 'soundcloud_streams', 'shazam_counts', 'explicit_track'
-];
-
-df[int_columns] = df[int_columns].replace({',': ''}, regex = True).astype(int)
+print(delimiter)
+print("Dataframe rows non-null:", df.shape[0])
+print("Dataframe cols non-null:", df.shape[1])
 
 # ver el tamaño minimo y maximo de columnas de texto
-print(delimiter);
-print("Text columns min length:");
-print(df.select_dtypes(include = ["object"]).apply(lambda x: x.str.len().min()));
+print(delimiter)
+print("Text columns min length:")
+print(df.select_dtypes(include=["object"]).apply(lambda x: x.str.len().min()))
 
-print(delimiter);
-print("Text columns max length:");
-print(df.select_dtypes(include = ["object"]).apply(lambda x: x.str.len().max()));
+print(delimiter)
+print("Text columns max length:")
+print(df.select_dtypes(include=["object"]).apply(lambda x: x.str.len().max()))
 
 # mostrar ejemplos de filas
-print(delimiter);
-print("Example rows:");
-print(df.sample(20));
+print(delimiter)
+print("Example rows:")
+print(df.sample(20))
 
 # guardar el dataframe limpio
-df.to_csv('./data/spotify_data_clean.csv', index = False);
+sample_df = df.sample(100000) #like the other dataset
+sample_df.to_csv("./data/spotify_data_clean.csv", index=False)
