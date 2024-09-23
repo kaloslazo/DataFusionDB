afeb077bfb8a0da6bc293a5e482dfa487e2abd9f
Lenin Chavez <102402199+Hyp3Boy@users.noreply.github.com>
2024-09-22 21:29:07 -0500

    feat: add first search approach for extendible hash

--- backend/ExtendibleHashing/main.cpp
+++ backend/ExtendibleHashing/main.cpp
@@ -3,15 +3,22 @@
 using namespace std;
 
 int main() {
-  // example instance with global depth 1 and bucket size 3, 2 rows in directory (0, 1)
+  // example instance with global depth 1 and bucket size 3, 2 rows in directory
+  // (0, 1)
   ExtendibleHashing<EHRecordA, string> eh(1, 3);
 
   eh.Create("./data/file_data.dat", "./data/file_buckets.dat");
   eh.Load_csv("../../data/spotify_data_clean.csv");
-  // eh.Print();
-  // eh.Search("MILLION DOLLAR BABY");
+  eh.Print();
 
-
+  optional<EHRecordA> record = eh.Search("2kMawcbk6PqMbcBzHBP9XO");
+  if (record.has_value()) {
+    cout << "[!] Found record" << endl;
+    record.value().Print();
+    cout << endl;
+  } else {
+    cout << "[!] Record not found" << endl;
+  }
 
   return 0;
 };
