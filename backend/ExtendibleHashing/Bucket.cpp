5920ae954bf0ee53c51afec639e1883b88b5c933
Lenin Chavez <102402199+Hyp3Boy@users.noreply.github.com>
2024-09-22 20:35:03 -0500

    refactor: make extendible hashing and its records implementation with templates

--- backend/ExtendibleHashing/Bucket.cpp
+++ backend/ExtendibleHashing/Bucket.cpp
@@ -1,21 +1,26 @@
 #include "Bucket.hpp"
-#include "Record.cpp"
+#include "ExtendibleHRecordA.hpp"
+#include "ExtendibleHRecordB.hpp"
 
-Bucket::Bucket(int max_keys) {
+template <class RECORD, class TK>
+Bucket<RECORD, TK>::Bucket(int max_keys) {
   // initialize local depth with 1 and max bucket size (records in bucket)
   Local_depth = 1;
   Max_bucket_size = max_keys;
   
   // create empty array of max_keys records
   Current_size = 0;
-  Records = new Record[max_keys];
+  Records = new RECORD[Max_bucket_size];
 };
 
-Bucket::~Bucket() {
+template <class RECORD, class TK>
+Bucket<RECORD, TK>::~Bucket() {
   delete[] Records;
 };
 
-int Bucket::Insert(Record& record) {
+
+template <class RECORD, class TK>
+int Bucket<RECORD, TK>::Insert(RECORD& record) {
   // check if bucket is full
   if (Current_size == Max_bucket_size) {
     cout << "[!] Bucket is full" << endl;
@@ -29,10 +34,11 @@ int Bucket::Insert(Record& record) {
   return 0;
 };
 
-void Bucket::Remove(char key[12]) {
+template <class RECORD, class TK>
+void Bucket<RECORD, TK>::Remove(TK key) {
   // search for record in bucket (o(k^2))
   for (int i = 0; i < Current_size; i++) {
-    if (Records[i].isrc == key) {
+    if (Records[i].key() == key) {
       // remove record from bucket shifting all records to the left (new space complexity O(k))
       for (int j = i; j < Current_size - 1; j++) {
         Records[j] = Records[j + 1];
@@ -43,7 +49,8 @@ void Bucket::Remove(char key[12]) {
   };
 };
 
-void Bucket::Print() {
+template <class RECORD, class TK>
+void Bucket<RECORD, TK>::Print() {
   cout << "Local depth: " << Local_depth << endl;
   cout << "Number of records: " << Current_size << endl;
 
--- backend/ExtendibleHashing/ExtendibleHashing.cpp
+++ backend/ExtendibleHashing/ExtendibleHashing.cpp
@@ -1,12 +1,15 @@
 #include "ExtendibleHashing.hpp"
-#include "Bucket.cpp"
 #include <cmath>
 #include <functional>
 #include <iostream>
+#include <typeinfo>
+#include "Bucket.cpp"
 
 using namespace std;
 
-ExtendibleHashing::ExtendibleHashing(int global_depth, int bucket_size) {
+template <class RECORD, class TK>
+ExtendibleHashing<RECORD, TK>::ExtendibleHashing(int global_depth,
+                                                 int bucket_size) {
   // instance global depth and number of cells 2^global_depth
   Global_depth = global_depth;
   Num_cells = pow(2, Global_depth);
@@ -14,11 +17,13 @@ ExtendibleHashing::ExtendibleHashing(int global_depth, int bucket_size) {
   Bucket_addresses.resize(Num_cells, -1);
 };
 
-ExtendibleHashing::~ExtendibleHashing() {
+template <class RECORD, class TK>
+ExtendibleHashing<RECORD, TK>::~ExtendibleHashing() {
   Close();
 };
 
-void ExtendibleHashing::Close() {
+template <class RECORD, class TK>
+void ExtendibleHashing<RECORD, TK>::Close() {
   // close files cuz we are done :)
   cout << "[!] Closing files" << endl;
 
@@ -34,17 +39,20 @@ void ExtendibleHashing::Close() {
 };
 
 // hash function converts key to hash value using binary approach
-int ExtendibleHashing::Hash(char key[12]) {
+template <class RECORD, class TK>
+int ExtendibleHashing<RECORD, TK>::Hash(TK key) {
   // convert key to hash using hash function (char*)
-  string str_key(key, 12);
-  hash<string> hash_str;
-  size_t key_hash = hash_str(str_key);
+  hash<TK> hash_key;
+  size_t key_hash = hash_key(key);
 
-  // return the global depth most significant bits of the key - index of the bucket in the directory
-  return key_hash & ((1 << Global_depth) - 1); 
+  // return the global depth most significant bits of the key - index of the
+  // bucket in the directory
+  return key_hash & ((1 << Global_depth) - 1);
 };
 
-void ExtendibleHashing::Create(string filename_data, string filename_buckets) {
+template <class RECORD, class TK>
+void ExtendibleHashing<RECORD, TK>::Create(string filename_data,
+                                           string filename_buckets) {
   // open file for save data and save buckets
   File_data.open(filename_data, ios::in | ios::out | ios::binary);
   File_buckets.open(filename_buckets, ios::in | ios::out | ios::binary);
@@ -52,7 +60,8 @@ void ExtendibleHashing::Create(string filename_data, string filename_buckets) {
   // check if files are open else create new files
   if (!File_data.is_open()) {
     cout << "[+] File not exists. Creating file " << filename_data << endl;
-    File_data.open(filename_data, ios::out | ios::in | ios::binary | ios::trunc);
+    File_data.open(filename_data,
+                   ios::out | ios::in | ios::binary | ios::trunc);
     File_data.close();
 
     File_data.open(filename_data, ios::in | ios::out | ios::binary);
@@ -60,29 +69,34 @@ void ExtendibleHashing::Create(string filename_data, string filename_buckets) {
 
   if (!File_buckets.is_open()) {
     cout << "[+] File not exists. Creating file " << filename_buckets << endl;
-    File_buckets.open(filename_buckets, ios::out | ios::in | ios::binary | ios::trunc);
+    File_buckets.open(filename_buckets,
+                      ios::out | ios::in | ios::binary | ios::trunc);
     File_buckets.close();
 
     File_buckets.open(filename_buckets, ios::in | ios::out | ios::binary);
   };
 
   // create empty two empty buckets with bucket size (handle 0 and 1)
-  Bucket* bucket_zero = new Bucket(Bucket_size);
-  Bucket* bucket_one = new Bucket(Bucket_size);
+  Bucket<RECORD, TK>* bucket_zero = new Bucket<RECORD, TK>(Bucket_size);
+  Bucket<RECORD, TK>* bucket_one = new Bucket<RECORD, TK>(Bucket_size);
 
-  // no hash needed, just save the buckets with address 0 and 1 (considering global depth = 1 always)
+  // no hash needed, just save the buckets with address 0 and 1 (considering
+  // global depth = 1 always)
   Save_bucket(bucket_zero, 0);
   Save_bucket(bucket_one, 1);
 
-  // save directory (with 0 and 1) to file, save num_cells * sizeof(int) bytes a bucket address
+  // save directory (with 0 and 1) to file, save num_cells * sizeof(int) bytes a
+  // bucket address
   File_data.seekp(0, ios::beg);
-  File_data.write((char*) (&Bucket_addresses[0]), Num_cells * sizeof(Bucket*));
-  
+  File_data.write((char*)(&Bucket_addresses[0]),
+                  Num_cells * sizeof(Bucket<RECORD, TK>*));
+
   File_data.flush();
 };
 
 // trying opening file for save data , if not return false
-bool ExtendibleHashing::Open(string filedata, string filebuckets) {
+template <class RECORD, class TK>
+bool ExtendibleHashing<RECORD, TK>::Open(string filedata, string filebuckets) {
   // open file for save data and save buckets
   File_data.open(filedata, ios::in | ios::out | ios::binary);
   File_buckets.open(filebuckets, ios::in | ios::out | ios::binary);
@@ -99,26 +113,31 @@ bool ExtendibleHashing::Open(string filedata, string filebuckets) {
 
   // load the bucket addresses from the file and save it in the directory
   File_data.seekg(0, ios::beg);
-  File_data.read((char*) (&Bucket_addresses[0]), Num_cells * sizeof(Bucket*));
+  File_data.read((char*)(&Bucket_addresses[0]),
+                 Num_cells * sizeof(Bucket<RECORD, TK>*));
 
   return true;
 };
 
-void ExtendibleHashing::Save_bucket(Bucket* bucket, int bucket_directory_index) {
+template <class RECORD, class TK>
+void ExtendibleHashing<RECORD, TK>::Save_bucket(Bucket<RECORD, TK>* bucket,
+                                                int bucket_directory_index) {
   // move to the end of the file for append new bucket
   // tellp return the position of current pointer
   File_buckets.seekp(0, ios::end);
   int bucket_offset = File_buckets.tellp();
 
   // save the bucket to the file
-  File_buckets.write((char*) bucket, sizeof(Bucket));
+  File_buckets.write((char*)bucket, sizeof(Bucket<RECORD, TK>));
   File_buckets.flush();
 
   // save the bucket address in the directory
   Bucket_addresses[bucket_directory_index] = bucket_offset;
 };
 
-Bucket* ExtendibleHashing::Load_bucket(int bucket_directory_index) {
+template <class RECORD, class TK>
+Bucket<RECORD, TK>* ExtendibleHashing<RECORD, TK>::Load_bucket(
+    int bucket_directory_index) {
   // get the bucket address from the directory
   int bucket_offset = Bucket_addresses[bucket_directory_index];
 
@@ -126,38 +145,41 @@ Bucket* ExtendibleHashing::Load_bucket(int bucket_directory_index) {
   File_buckets.seekg(bucket_offset, ios::beg);
 
   // read the bucket from the file
-  Bucket* bucket = new Bucket(Bucket_size);
-  File_buckets.read((char*) bucket, sizeof(Bucket));
+  Bucket<RECORD, TK>* bucket = new Bucket<RECORD, TK>(Bucket_size);
+  File_buckets.read((char*)bucket, sizeof(Bucket<RECORD, TK>));
 
   return bucket;
 };
 
 // insert record handling hash function and split bucket
-bool ExtendibleHashing::Insert(Record record) {
-  int bucket_directory_index = Hash(record.isrc);
+template <class RECORD, class TK>
+bool ExtendibleHashing<RECORD, TK>::Insert(RECORD record) {
+  int bucket_directory_index = Hash(record.key());
 
   while (true) {
-    Bucket* bucket = Load_bucket(bucket_directory_index);
+    Bucket<RECORD, TK>* bucket = Load_bucket(bucket_directory_index);
 
     if (bucket->Current_size < bucket->Max_bucket_size) {
       bucket->Insert(record);
-      cout << "[+] Inserted record and saved key " << record.isrc << " in bucket in position " << bucket_directory_index << endl;
+      cout << "[+] Inserted record and saved key " << record.key()
+           << " in bucket in position " << bucket_directory_index << endl;
       Save_bucket(bucket, bucket_directory_index);
       return true;
     }
 
     cout << "[!] Bucket is full. Splitting bucket" << endl;
-    Split_bucket(record.isrc, bucket_directory_index);
+    Split_bucket(record.key(), bucket_directory_index);
 
     // Recompute bucket index after splitting
-    bucket_directory_index = Hash(record.isrc);
+    bucket_directory_index = Hash(record.key());
   }
 }
 
-
 // handle split bucket and expand directory
-void ExtendibleHashing::Split_bucket(char key[12], int bucket_directory_index) {
-  Bucket* old_bucket = Load_bucket(bucket_directory_index);
+template <class RECORD, class TK>
+void ExtendibleHashing<RECORD, TK>::Split_bucket(TK key,
+                                                 int bucket_directory_index) {
+  Bucket<RECORD, TK>* old_bucket = Load_bucket(bucket_directory_index);
 
   // if local depth is equal to global depth, expand directory
   if (old_bucket->Local_depth == Global_depth) {
@@ -169,23 +191,25 @@ void ExtendibleHashing::Split_bucket(char key[12], int bucket_directory_index) {
   old_bucket->Local_depth++;
 
   // create new bucket with the same max size
-  Bucket* new_bucket = new Bucket(old_bucket->Max_bucket_size);
+  Bucket<RECORD, TK>* new_bucket =
+      new Bucket<RECORD, TK>(old_bucket->Max_bucket_size);
   new_bucket->Local_depth = old_bucket->Local_depth;
 
   // calculate new bucket index
-  int new_bucket_index = bucket_directory_index | (1 << (old_bucket->Local_depth - 1));
+  int new_bucket_index =
+      bucket_directory_index | (1 << (old_bucket->Local_depth - 1));
 
   // redistribute the records between the two buckets (old and new)
   cout << "[+] Redistributing records between buckets" << endl;
 
   for (int i = 0; i < old_bucket->Current_size; i++) {
-    Record* record = &old_bucket->Records[i];
-    int hash = Hash(record->isrc);
+    RECORD* record = &old_bucket->Records[i];
+    int hash = Hash(record->key());
 
     if ((hash & ((1 << old_bucket->Local_depth) - 1)) == new_bucket_index) {
       new_bucket->Insert(*record);
-      old_bucket->Remove(record->isrc);
-      i--; // Adjust index because we removed an element
+      old_bucket->Remove(record->key());
+      i--;  // Adjust index because we removed an element
     }
   }
 
@@ -202,7 +226,8 @@ void ExtendibleHashing::Split_bucket(char key[12], int bucket_directory_index) {
 };
 
 // expand directory (double the size of the directory)
-void ExtendibleHashing::Expand_directory() {
+template <class RECORD, class TK>
+void ExtendibleHashing<RECORD, TK>::Expand_directory() {
   // double size of directory
   int new_num_cells = 2 * Num_cells;
   vector<int> new_bucket_addresses(new_num_cells);
@@ -219,24 +244,27 @@ void ExtendibleHashing::Expand_directory() {
 
   // Update the file with the new directory
   File_data.seekp(0, ios::beg);
-  File_data.write((char*) (&Bucket_addresses[0]), Num_cells * sizeof(int));
+  File_data.write((char*)(&Bucket_addresses[0]), Num_cells * sizeof(int));
   File_data.flush();
 }
 
 // print all records
-void ExtendibleHashing::Print() {
+template <class RECORD, class TK>
+void ExtendibleHashing<RECORD, TK>::Print() {
   cout << endl << "[!] Print directory with " << Num_cells << " cells" << endl;
-  
+
   for (int i = 0; i < Num_cells; i++) {
-    cout << "[+] Cell " << i << " with bucket address " << Bucket_addresses[i] << endl;
-    Bucket* bucket = Load_bucket(i);
+    cout << "[+] Cell " << i << " with bucket address " << Bucket_addresses[i]
+         << endl;
+    Bucket<RECORD, TK>* bucket = Load_bucket(i);
     bucket->Print();
     cout << endl;
   };
 };
 
 // load from csv and insert into extendible hashing
-void ExtendibleHashing::Load_csv(string filename) {
+template <class RECORD, class TK>
+void ExtendibleHashing<RECORD, TK>::Load_csv(string filename) {
   ifstream file(filename, ios::in);
   if (!file.is_open()) {
     cout << "[!] Error. Can't open file: " << filename << endl;
@@ -244,18 +272,14 @@ void ExtendibleHashing::Load_csv(string filename) {
   }
 
   string line;
-  getline(file, line); // skip header
+  getline(file, line);  // skip header
 
   cout << "[!] Loading data from file " << filename << endl;
   while (getline(file, line)) {
-    char track[100], album_name[100], artist[50], release_date[10], isrc[12];
-    int all_time_rank = 0, track_score = 0, spotify_streams = 0, spotify_popularity = 0;
-    int youtube_views = 0, tiktok_views = 0, airplay_spins = 0, siriusmxm_spins = 0;
-    int pandora_streams = 0, soundcloud_streams = 0, shazam_counts = 0, explicit_tracks = 0;
-
+    // one by each RECORD class atributes
+    RECORD record;
     stringstream ss(line);
     string token;
-
     auto safe_stoi = [](const string& str, int default_value = 0) -> int {
       try {
         return str.empty() ? default_value : stoi(str);
@@ -265,29 +289,61 @@ void ExtendibleHashing::Load_csv(string filename) {
         return default_value;
       }
     };
+    // if (typeid(RECORD) == typeid(EHRecordA)) {
+    char id[23];
+    char name[521];
+    char album[244];
+    char album_id[23];
+    char artists[1124];
+    getline(ss, token, ',');
+    strncpy(id, token.c_str(), sizeof(id) - 1);
+    id[sizeof(id) - 1] = '\0';
+    getline(ss, token, ',');
+    strncpy(name, token.c_str(), sizeof(name) - 1);
+    name[sizeof(name) - 1] = '\0';
+    getline(ss, token, ',');
+    strncpy(album, token.c_str(), sizeof(album) - 1);
+    album[sizeof(album) - 1] = '\0';
+    getline(ss, token, ',');
+    strncpy(album_id, token.c_str(), sizeof(album_id) - 1);
+    album_id[sizeof(album_id) - 1] = '\0';
+    getline(ss, token, ',');
+    strncpy(artists, token.c_str(), sizeof(artists) - 1);
+    artists[sizeof(artists) - 1] = '\0';
+    cout << "ID: " << id << endl;
 
-    getline(ss, token, ','); strncpy(track, token.c_str(), sizeof(track) - 1); track[sizeof(track) - 1] = '\0';
-    getline(ss, token, ','); strncpy(album_name, token.c_str(), sizeof(album_name) - 1); album_name[sizeof(album_name) - 1] = '\0';
-    getline(ss, token, ','); strncpy(artist, token.c_str(), sizeof(artist) - 1); artist[sizeof(artist) - 1] = '\0';
-    getline(ss, token, ','); strncpy(release_date, token.c_str(), sizeof(release_date) - 1); release_date[sizeof(release_date) - 1] = '\0';
-    getline(ss, token, ','); strncpy(isrc, token.c_str(), sizeof(isrc) - 1); isrc[sizeof(isrc) - 1] = '\0';
+    record = RECORD(id, name, album, album_id, artists);
+    record.Print();
 
-    getline(ss, token, ','); all_time_rank = safe_stoi(token);
-    getline(ss, token, ','); track_score = safe_stoi(token);
-    getline(ss, token, ','); spotify_streams = safe_stoi(token);
-    getline(ss, token, ','); spotify_popularity = safe_stoi(token);
-    getline(ss, token, ','); youtube_views = safe_stoi(token);
-    getline(ss, token, ','); tiktok_views = safe_stoi(token);
-    getline(ss, token, ','); airplay_spins = safe_stoi(token);
-    getline(ss, token, ','); siriusmxm_spins = safe_stoi(token);
-    getline(ss, token, ','); pandora_streams = safe_stoi(token);
-    getline(ss, token, ','); soundcloud_streams = safe_stoi(token);
-    getline(ss, token, ','); shazam_counts = safe_stoi(token);
-    getline(ss, token, ','); explicit_tracks = safe_stoi(token);
+    Insert(record);
+    // } else if (typeid(RECORD) == typeid(EHRecordB)) {
+    //   int year;
+    //   char make[30];
+    //   char model[50];
+    //   char vin[18];
+    //   getline(ss, token, ',');
+    //   // to int year
+    //   year = stoi(token);
+    //   getline(ss, token, ',');
+    //   strncpy(make, token.c_str(), sizeof(make) - 1);
+    //   make[sizeof(make) - 1] = '\0';
+    //   getline(ss, token, ',');
+    //   strncpy(model, token.c_str(), sizeof(model) - 1);
+    //   model[sizeof(model) - 1] = '\0';
+    //   getline(ss, token, ',');
+    //   strncpy(vin, token.c_str(), sizeof(vin) - 1);
+    //   vin[sizeof(vin) - 1] = '\0';
+    //   getline(ss, token, ',');
+    //   strncpy(vin, token.c_str(), sizeof(vin) - 1);
+    //   vin[sizeof(vin) - 1] = '\0';
 
-    Record new_record(track, album_name, artist, release_date, isrc, all_time_rank, track_score, spotify_streams, spotify_popularity, youtube_views, tiktok_views, airplay_spins, siriusmxm_spins, pandora_streams, soundcloud_streams, shazam_counts, explicit_tracks);
-    new_record.Print();
+    //   record = RECORD(year, make, model, vin);
 
-    Insert(new_record);
+    //   record.Print();
+
+    //   Insert(record);
+
+    //   return;
+    // }
   }
 }
