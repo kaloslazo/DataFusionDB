50284f7111c32289cf1d0b97295f4e51c52d2890
Kalos Lazo <lzm.kalos@gmail.com>
2024-09-24 21:02:51 -0500

    feat: funciona extendible xd

---
+++ backend/Bucket.hpp
@@ -0,0 +1,23 @@
+#ifndef BUCKET_HPP
+#define BUCKET_HPP
+
+#include <optional>
+
+template <class RECORD, class TK>
+class Bucket {
+public:
+    int Local_depth;
+    int Max_bucket_size;
+    int Current_size;
+    RECORD* Records;
+
+    Bucket(int max_keys);
+    ~Bucket();
+
+    int Insert(RECORD& record);
+    std::optional<RECORD> Search(TK key);
+    void Remove(TK key);
+    void Print();
+};
+
+#endif // BUCKET_HPP
---
+++ backend/ExtendibleHashing.hpp
@@ -0,0 +1,40 @@
+#ifndef EXTENDIBLEHASHING_HPP
+#define EXTENDIBLEHASHING_HPP
+
+#include <vector>
+#include <fstream>
+#include <optional>
+#include <string>
+#include <cstring>
+#include "Bucket.hpp"
+
+template <class RECORD, class TK>
+class ExtendibleHashing {
+private:
+    int Global_depth;
+    int Num_cells;
+    int Bucket_size;
+    std::vector<int> Bucket_addresses;
+    std::fstream File_data;
+    std::fstream File_buckets;
+
+    void Save_bucket(Bucket<RECORD, TK>* bucket, int bucket_directory_index);
+    Bucket<RECORD, TK>* Load_bucket(int bucket_directory_index);
+    void Split_bucket(TK key, int bucket_directory_index);
+    void Expand_directory();
+
+public:
+    ExtendibleHashing(int global_depth, int bucket_size);
+    ~ExtendibleHashing();
+
+    void Close();
+    int Hash(TK key);
+    void Create(std::string filename_data, std::string filename_buckets);
+    bool Open(std::string filedata, std::string filebuckets);
+    bool Insert(RECORD record);
+    std::optional<RECORD> Search(TK key);
+    void Print();
+    void Load_csv(std::string filename);
+};
+
+#endif // EXTENDIBLEHASHING_HPP
